3e4643988f754d9069b8bab770decefcbccfcf5a
tri <tridesignnm@gmail.com>
2022-07-23 07:40:36 +0700

    da xong 4 erquest can ban

--- routes/genres.route.js
+++ routes/genres.route.js
@@ -4,11 +4,12 @@ const Joi = require("joi");
 const mongoose = require("mongoose");
 
 const schemaGenres = new mongoose.Schema({
-  name: { type: String, require: true },
+  name: { type: String, required: true },
 });
 
 const GenresModel = mongoose.model("Video", schemaGenres);
 
+//get /api/genres/:id
 route.get("/:id", (req, res) => {
   GenresModel.findById(req.params.id)
     .then((video) => {
@@ -20,8 +21,55 @@ route.get("/:id", (req, res) => {
     });
 });
 
+//delete /api/genres
+route.delete("/:id", (req, res) => {
+  GenresModel.findByIdAndRemove(req.params.id)
+    .then((video) => {
+      res.send(video);
+      console.log("da xoa thanh cong", video);
+    })
+    .catch((err) => res.status(404).send(err.message));
+});
+
+//post /api/genres/
+route.post("/", (req, res) => {
+  const { error, value } = validateGenres(req.body);
+
+  if (error) return res.status(404).send(error.details[0].message);
+  GenresModel.create(req.body)
+    .then((video) => {
+      console.log(video);
+      res.send(video);
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(404).send(err.message);
+    });
+});
+
+//get /api/genres
 route.get("/", (req, res) => {
-  res.send("this is genres page");
+  GenresModel.find()
+    .then((videos) => {
+      res.send(videos);
+    })
+    .catch((err) => res.status(500).send(err.message));
+});
+
+//patch /api/genres/:id
+route.put("/:id", (req, res) => {
+  GenresModel.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    { new: true }
+  )
+    .then((video) => {
+      console.log(video);
+      res.send(video);
+    })
+    .catch((err) => res.status(404).send(err.message));
 });
 
 module.exports = route;
